d3259378d603407f4e9742c0e00dc88344a331ad
Will Barnes <wtb2@rice.edu>
2015-12-12 03:01:49 -0600

    unpacked GUI java source; only for use in mapping config.h variables

---
+++ HYDRAD_GUI/source/CHeatingEvent.java
@@ -0,0 +1,12 @@
+package GUI;
+
+public class CHeatingEvent
+{
+  public double fVHR;
+  public double fSL;
+  public double fSS;
+  public double fOT;
+  public double fTD;
+  public double fTSRP;
+  public double fTSDP;
+}
