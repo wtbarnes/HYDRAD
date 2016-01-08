5093a0423a46356eb35a4e28fb2d4492633052ed
Will Barnes <wtb2@rice.edu>
2016-01-08 01:14:23 -0600

    correcting print statement with string

--- Resources/source/xmlreader.cpp
+++ Resources/source/xmlreader.cpp
@@ -20,7 +20,7 @@ TiXmlElement * check_element(TiXmlElement *result,std::string search_value)
 	{
 		//TODO:default value when value cannot be found; search for default configuration.
 		//Package default XML with HYDRAD
-		printf("Could not find %s.\n",search_value);
+		printf("Could not find %s.\n",search_value.c_str());
 		printf("Returning NULL.\n");
 		return NULL;
 	}
